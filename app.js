7a9142e162170fec00f58c93a206da6db94fbaf5
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-01-12 13:30:19 -0800

    connect DOM elements to js

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 // import functions and grab DOM elements
 
+const numberInput = document.getElementById('numberInput');
+const guessBtn = document.getElementById('guessBtn');
+const remainingGuesses = document.getElementById('remainingGuesses');
+const highLowWrong = document.getElementById('highLowWrong');
+
+
 // initialize state
+let winningNumber = Math.ceil(Math.random() * 20);
+let remainGuessState = 4;
 
 // set event listeners to update state and DOM
+console.log(remainGuessState);
 
-const winningNumber = Math.ceil(Math.random() * 20);
 
+
+/*
 const function comapre(guess, winningNumber) {
     return 0
         - 1
     or 1 to decide which result to show
 }
+
+*/
