1e554c27ac8ab5ced6bd5ecf5493e8dbbebdd73e
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-01-12 15:24:58 -0800

    add try again button with reset functionality

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ const numberInput = document.getElementById('numberInput');
 const guessBtn = document.getElementById('guessBtn');
 const remainingGuesses = document.getElementById('remainingGuesses');
 const highLowWrong = document.getElementById('highLowWrong');
+const tryAgain = document.getElementById('tryAgain');
 
 
 // initialize state
 let winningNumber = Math.ceil(Math.random() * 20);
 let remainGuessState = 4;
-console.log(winningNumber);
 
 // set event listeners to update state and DOM
 guessBtn.addEventListener('click', () => {
+    console.log(winningNumber);
     remainGuessState--;
     remainingGuesses.textContent = `You have ${remainGuessState} guesses remaining!`;
     let userGuess = numberInput.value;
@@ -35,6 +36,16 @@ guessBtn.addEventListener('click', () => {
         remainingGuesses.hidden = true;
         numberInput.disabled = true;
         guessBtn.disabled = true;
+        tryAgain.style.visibility = 'visible';
     }
+});
 
+tryAgain.addEventListener('click', () => {
+    remainGuessState = 4;
+    winningNumber = Math.ceil(Math.random() * 20);
+    remainingGuesses.hidden = false;
+    numberInput.disabled = false;
+    guessBtn.disabled = false;
+    tryAgain.style.visibility = 'hidden';
+    highLowWrong.textContent = `Give it another go!`;
 });
