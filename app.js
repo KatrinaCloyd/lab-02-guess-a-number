962c52b32dbe1fdd79310d3acaa025e79419792c
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-01-12 13:16:01 -0800

    create html elements

--- app.js
+++ app.js
@@ -3,3 +3,11 @@
 // initialize state
 
 // set event listeners to update state and DOM
+
+const winningNumber = Math.ceil(Math.random() * 20);
+
+const function comapre(guess, winningNumber) {
+    return 0
+        - 1
+    or 1 to decide which result to show
+}
